f8ab41a35015ed4cd458aecec3caefe2e31e86e6
smackdh <smackdh@gmail.com>
2023-08-10 22:44:10 +0900

    test

--- pwdgen/generator/tests.py
+++ pwdgen/generator/tests.py
@@ -6,5 +6,5 @@ class SimpleTestCase(TestCase):
     def test_home_page(self):
         response = self.client.get(
             reverse("generator")
-        )  # Replace 'home' with the name of your view's URL
+        )  # Replace 'home' with the name of your view's URLsss
         self.assertEqual(response.status_code, 200)
