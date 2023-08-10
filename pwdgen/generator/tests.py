df10bde0db6a997a9b506e94059325c9d7bf67c1
smackdh <smackdh@gmail.com>
2023-08-10 22:43:07 +0900

    added small test

--- pwdgen/generator/tests.py
+++ pwdgen/generator/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SimpleTestCase(TestCase):
+    def test_home_page(self):
+        response = self.client.get(
+            reverse("generator")
+        )  # Replace 'home' with the name of your view's URL
+        self.assertEqual(response.status_code, 200)
