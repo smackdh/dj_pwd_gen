b0b42e18cf856f77dae1f9d336e7cd29eab7943a
Mattias <mattiaswelamsson@gmail.com>
2023-08-12 15:30:03 +0900

    add elpise background color

--- pwdgen/pwdgen/settings.py
+++ pwdgen/pwdgen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-gm3t24-e&c9ka+&754s8=%!ozl8!n(_j7_ms7be0y(07__66q6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
