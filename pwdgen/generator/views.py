31adbf8751d7d1a45bf54b6ecff9496672756b49
Mattias <mattiaswelamsson@gmail.com>
2023-08-12 12:55:18 +0900

    added first static file

--- pwdgen/generator/views.py
+++ pwdgen/generator/views.py
@@ -6,3 +6,14 @@ def generator(request):
     text = "HI THERE"
     context = {"text": text}
     return render(request, "generator.html", context)
+
+
+# #- Online Python - IDE, Editor, Compiler, Interpreter
+# import random
+# import string
+# def pwdgen(length):
+#    return length
+# characters = list(string.ascii_letters)
+# print(characters)
+# numbers = list(string.digits)
+# print(numbers)
