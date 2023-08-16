d5a84768392e4e1322920a350f19e0249169e871
phatsta <daniel@leetcom.se>
2023-08-16 16:45:16 +0900

    Set up Gunicorn and configure for Heroku

--- pwdgen/pwdgen/settings.py
+++ pwdgen/pwdgen/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "productionfiles"
 
 STATIC_URL = "static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 
