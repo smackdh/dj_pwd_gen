89094946122f1c038564d62d2808e1be49bb7fc3
phatsta <daniel@leetcom.se>
2023-08-16 18:20:26 +0900

    vercel

--- pwdgen/pwdgen/wsgi.py
+++ pwdgen/pwdgen/wsgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pwdgen.pwdgen.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pwdgen.settings")
 
 application = get_wsgi_application()
 app = application
