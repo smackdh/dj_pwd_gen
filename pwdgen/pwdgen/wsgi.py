2c5521b3085ce4998f48371a9d8ec5393c0c78c0
phatsta <daniel@leetcom.se>
2023-08-16 17:18:26 +0900

    procfile test

--- pwdgen/pwdgen/wsgi.py
+++ pwdgen/pwdgen/wsgi.py
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-from pwdgen import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pwdgen.settings")
 
